27935b23139e431e71419079a9ca47ccdac65be0
JayakarAddepalli <addepallijayakar@gmail.com>
2025-01-27 21:03:21 +0530

    added the vercel.json and requirements.txt file to deploy the application

--- DEV/settings.py
+++ DEV/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-u8d%vv&qq^2qa1vazljwwhmj85q##y(jyrh8nxsr@&26^ta!+1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
